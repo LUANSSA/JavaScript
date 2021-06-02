5d70ff3992b93cdadfd1e72f9024a2a69d0816e1
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-06-02 11:45:19 -0300

    Update JS04_BooleanCondicionais.js

--- JS04_BooleanCondicionais.js
+++ JS04_BooleanCondicionais.js
@@ -43,12 +43,53 @@ if(varExpBoolean){
 console.log(num1Qualquer); //var CONTINUA A EXISTIR FORA DO BLOCO.
 
 //------------------------------------------------------------------------------
-if(letExpBoolean){
-  console.log("True!");
-  let num2Qualquer = 10; //SÓ PASSA A EXISTIR DENTRO DO BLOCO SE letExpBoolean FOR true
+
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+var idade = 24;
+var primo = 18;
+if(idade < primo){
+  console.log("Maior que o primo");
+}else if(idade === primo){
+  console.log("Mesma idade do primo");
 }else{
-  console.log("False!");
+  console.log("Mais novo que o primo");
 }
-//console.log(num2Qualquer); ERRO! POIS num2Qualquer SÓ EXISTE DENTRO DO BLOCO
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+console.log(expressao);
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre';
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+console.log(!!nome);
+console.log(!!idade);
+console.log(!!possuiDoutorado);
+console.log(!!empregoFuturo);
+console.log(!!dinheiroNaConta);
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+var DiferencaBrasilChina = (china-brasil)*1000000000;
+console.log(DiferencaBrasilChina);
+
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Gato != gato');
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+  //Vai aparecer Cão pois no caso de comparação com && o último verdadeiro é que é impresso
+} else {
+  console.log('Falso');
+}
 
 //------------------------------------------------------------------------------
