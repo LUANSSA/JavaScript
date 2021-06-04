491f8ff8aa606dab1f626e5260eca11565f5a3cc
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-06-04 11:03:02 -0300

    Update JS05_function.js

--- JS05_function.js
+++ JS05_function.js
@@ -1,3 +1,4 @@
+//CALCULANDO ÁREA DO QUADRADO
 function areaQuadrado (lado){
   return "função areaQuadrado ativa\n"+"area: "+lado*lado;
 
@@ -6,12 +7,14 @@ console.log(areaQuadrado(2));//imprime 4
 console.log(areaQuadrado(4));//imprime 16
 console.log(areaQuadrado(8));//imprime 64
 
+//CALCULANDO IMC
 function imc(peso, altura){
   let imc = peso/ (altura**2);
   return "funcão imc ativa\n"+"imc: "+imc.toFixed(2);
 }
 console.log(imc(80,1.8));
 
+//VERIFICANDO A COR FAVORITA
 function corFavorita(cor){
   (cor)? cor=cor.toLowerCase():cor; // verifica se cor é true e caso seja utiliza a função toLowerCase()
   switch(cor){
@@ -24,3 +27,17 @@ function corFavorita(cor){
 console.log(corFavorita("azul"));
 console.log(corFavorita("verde"))
 console.log(corFavorita());
+
+//FUNÇÕES COM FUNÇÕES COMO ARGUMENTOS
+
+//PRIMEIRO JEITO DE SE FAZER
+function mostrarConsole(){
+  console.log("função mostrarConsole ativa");
+}
+addEventListener("mouseover",mostrarConsole);
+
+//SEGUNDO JEITO DE SE FAZER
+//PASSANDO UMA FUNÇÃO ANÔNIMA COMO ARGUMENTO
+addEventListener("click",function(){console.log("função addEventListener ativa")});
+
+
