27103b5c23bee0c7bcaeef2be334cc9e349ffbfa
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-06-10 11:11:16 -0300

    Create JS08_functionArrow.js

---
+++ JS08_functionArrow.js
@@ -0,0 +1,42 @@
+//ARROW FUNCTION
+// => significa 'vai para'
+
+//Primeiro exemplo - concise body - o "return é implícito"
+
+var funcFristExemple = x => x*x;          // x*x é retornado implícitamente
+console.log(funcFristExemple);
+console.log(funcFristExemple());          // [Function: funcFristExemple]
+console.log(funcFristExemple(0));         // NaN
+console.log(funcFristExemple(10));        // 100
+console.log(funcFristExemple(10,5));      // 100
+console.log(funcFristExemple(10,5,5));    // 100
+
+var funcFristExemple = (x,y) => x*y;       //x*y é retornado implícitamente
+console.log(funcFristExemple);            // [Function: funcFristExemple]
+console.log(funcFristExemple());          // NaN
+console.log(funcFristExemple(0));         // NaN
+console.log(funcFristExemple(10));        // NaN
+console.log(funcFristExemple(10,5));      // 50
+console.log(funcFristExemple(10,5,5));    // 50
+console.log(funcFristExemple(10,5,5,5));  // 50
+
+var funcFristExemple = (...args) => args; // args é retornado implícitamente
+console.log(funcFristExemple);            // [Function: funcFristExemple]
+console.log(funcFristExemple());          // []
+console.log(funcFristExemple(0));         // [ 0 ]
+console.log(funcFristExemple(10));        // [ 10 ]
+console.log(funcFristExemple(10,5));      // [ 10, 5 ]
+console.log(funcFristExemple(10,5,5));    // [ 10, 5, 5 ]
+console.log(funcFristExemple(10,5,5,5));  // [ 10, 5, 5, 5 ]
+
+ //x*x retorna implícitamente para args.map e args.map retorna implícitamente
+var funcFristExemple = (...args) => args.map(x => x*x);
+console.log(funcFristExemple);            // [Function: funcFristExemple]
+console.log(funcFristExemple());          // []
+console.log(funcFristExemple(0));         // [ 0 ]
+console.log(funcFristExemple(10));        // [ 100 ]
+console.log(funcFristExemple(10,5));      // [ 100, 25 ]
+console.log(funcFristExemple(10,5,5));    // [ 100, 25, 25 ]
+console.log(funcFristExemple(10,5,5,5));  // [ 100, 25, 25, 25 ]
+ //Segundo exemplo
+ 
