2c52cada800c668b29990f1dcc5b795882d12515
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2021-06-12 23:20:17 -0300

    Update JS09_Object.js

--- JS09_Object.js
+++ JS09_Object.js
@@ -8,3 +8,10 @@ var a = {
 }
 console.log(a); // { nome: 'Luan', idade: 24, profissao: 'Desenvolvedor de Sistemas' }
 
+const b = {} // Object
+b = {
+  nome:"LUAN",
+  idade:24,
+  profissao:"Desenvolvimento de Sistemas"
+}
+
