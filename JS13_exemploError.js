67352832cf840f85907bd1a65c3c0b337cdd63f3
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2022-04-29 17:54:53 -0300

    Set, Map, DOMException, ECMAScript

---
+++ JS13_exemploError.js
@@ -0,0 +1,37 @@
+//verificando possiveis erros ao enviar um array e um número
+function verificaArray(array, num){
+    try{    
+        if(!array && !num) throw new ReferenceError("Envie os parametros");
+    
+        if(typeof array !== "object") throw new TypeError("Envie um objeto");
+    
+        if(typeof num !== "number") throw new TypeError("Envie um number");
+
+        if(array.length !== num) throw new RangeError("Tamanho inválido");
+        
+        //caso o array seja válido. Retorna-lo
+        return array;
+    }
+    catch(e){
+        if(e instanceof ReferenceError){
+            console.log("Este erro é um ReferenceError!");
+            console.log("Mensagem: ", e.message);
+            console.log("Nome: ", e.name);
+            console.log("Pilha: ", e.stack);
+        }
+        else if(e instanceof TypeError){
+            console.log("Este erro é um TypeError!");
+            console.log("Mensagem: ", e.message);
+            console.log("Nome: ", e.name);
+            console.log("Pilha: ", e.stack);
+        }
+        else if(e instanceof RangeError){
+            console.log("Este erro é um RangeError");
+            console.log("Mensagem: ", e.message);
+            console.log("Nome: ", e.name);
+            console.log("Pilha: ", e.stack);
+        }
+    }
+}
+
+verificaArray();
