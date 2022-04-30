8a9ac7c02434292c803ac0429cb52d745072c762
LUANSSA <54681373+LUANSSA@users.noreply.github.com>
2022-04-30 17:39:24 -0300

    async and await

---
+++ JS19_asyncAwait.js
@@ -0,0 +1,47 @@
+//JS00_index ReferenceError
+
+/*
+    Embelezador sintático
+    Async/Await é uma forma mais bonita de se enxergar Promise
+
+    Promise é uma maneira diferente de se executar funções Callback
+
+    função Callback é uma função que será executada quando um
+    processo assíncrono for executado.  
+*/
+
+const fs = require("fs");
+
+//criando uma Promise
+const readFile = file => new Promise((resolve, reject) =>{
+    fs.readFile(file, (err, contents)=>{
+        if(err){
+            reject(err);
+        }
+        else{
+            resolve(contents);
+        }
+    });
+});
+
+console.log(readFile("nameList00.txt"));
+//Promise { <pending> }
+
+//Async/Await é um embelezador sintático de Promise
+const initOne = async() =>{
+
+}
+initOne();
+//
+console.log(initOne());
+//Promise { undefined } - está undefined pois não tem nada escrito no bloco da função async() =>{}
+
+const initTwo = async() =>{
+    console.log("funcionou!");
+}
+initTwo();
+//funcionou!
+console.log(initTwo());
+//funcionou!
+//funcionou!
+//Promise { undefined }
