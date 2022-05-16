f031c42e439e3c3c91fa3b911dd1ce008c9297b8
Luan Souza <alunoluansenai@gmail.com>
2022-05-15 21:41:16 -0300

    Template String

---
+++ JS60_templateString.js
@@ -0,0 +1,9 @@
+let nome = "Luan";
+let sobrenome = "Souza";
+let profissao = "Desenvolvedor Front-end";
+
+//Sem template String
+console.log("Olá,", nome, sobrenome, "\nVocê é", profissao);
+
+//Com template String
+console.log(`Olá, ${nome} ${sobrenome}\nVocê é ${profissao}`);
