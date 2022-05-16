ea0287a4ac8b4647342f6f57cd1cafa3914364ed
Luan Souza <alunoluansenai@gmail.com>
2022-05-16 14:23:09 -0300

    Split - Sort - Join

---
+++ JS67_splitSortJoin.js
@@ -0,0 +1,5 @@
+let exemplo = "node";
+console.log(exemplo)//node
+console.log(exemplo.split(""));//[ 'n', 'o', 'd', 'e' ]
+console.log(exemplo.split("").sort());//[ 'd', 'e', 'n', 'o' ]
+console.log(exemplo.split("").sort().join(""));//deno
