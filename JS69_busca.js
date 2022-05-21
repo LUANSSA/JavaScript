7b38c3307faba0dbf02c2edb6a3d3547d75c8b99
Luan Souza <alunoluansenai@gmail.com>
2022-05-21 19:23:01 -0300

    Busca

---
+++ JS69_busca.js
@@ -0,0 +1,14 @@
+let valores = [5, 8, 10, 22, 45, 38];
+
+function busca(num, vetor){
+    let tam = vetor.length;
+    for(let i = 0; i < tam; i++){
+        if(num == valores[i]) return i;
+    }
+    return -1;
+}
+
+console.log(valores);
+console.log(busca(5, valores));
+console.log(busca(10, valores));
+console.log(busca(50, valores));
