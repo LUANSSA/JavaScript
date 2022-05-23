e113fe82be7b254a35f2e24de434e3318cfd8b18
Luan Souza <alunoluansenai@gmail.com>
2022-05-23 17:50:25 -0300

    Boolean e Condicionais atualizado

--- JS04_BooleanCondicionais.js
+++ JS04_BooleanCondicionais.js
@@ -1,86 +1,78 @@
-var varExpBoolean = true;
-let letExpBoolean = true;
-
-/*
+//Todas essas condições são falsas
 if(false){}                 //false
 if(0 || -0){}               //false
 if(NaN){}                   //false
 if(null){}                  //false
 if(undefined){}             //false
 if('' || "" || ``){}        //false
-*/
 
-/*
+
+//Todas essas condições são verdadeiras
 if(true){}                  //true
 if(1 || 2){}                //true
 if(" "){}                   //true
 if(-5 || -4){}              //true
 if("LUAN"){}                //true
 if({}){}                    //true
-*/
 
+
+//Condição false
 if(""){
-  console.log("\"\" True");
+  console.log("\"\"", true);
 }else{
-  console.log("\"\" False");
+  console.log("\"\"", false);
+  //"" false
 }
+
+//Condição verdadeira
 if("Luan"){
   console.log("\"Luan\"", true);
+  //"Luan" true
 }else{
   console.log("\"Luan\"", false);
 }
 
-//------------------------------------------------------------------------------
-if(varExpBoolean){
-  console.log("True!");
-  var num1Qualquer = 10; //MESMO QUE NÃO ENTRE NO if numQualquer PASSA A EXISTIR
-}else{
-  console.log("False!");
-}
 
-//CASO varExpBoolean FOSSE false num1Qualquer EXISTIRIA SENDO undefined
-
-console.log(num1Qualquer); //var CONTINUA A EXISTIR FORA DO BLOCO.
-
-//------------------------------------------------------------------------------
-
-// Verifique se a sua idade é maior do que a de algum parente
-// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+// Verifique se a sua idade é maior do que a idade do primo
 var idade = 24;
-var primo = 18;
-if(idade < primo){
-  console.log("Maior que o primo");
-}else if(idade === primo){
+var primoIdade = 18;
+if(idade > primoIdade){
+  console.log("Tem mais idade que o primo");
+  //Tem mais idade que o primo
+}else if(idade === primoIdade){
   console.log("Mesma idade do primo");
 }else{
   console.log("Mais novo que o primo");
 }
-// Qual valor é retornado na seguinte expressão?
-var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
-console.log(expressao);
-
-// Verifique se as seguintes variáveis são Truthy ou Falsy
-var nome = 'Andre';
-var idade = 28;
-var possuiDoutorado = false;
-var empregoFuturo;
-var dinheiroNaConta = 0;
-console.log(!!nome,!!idade,!!possuiDoutorado,!!empregoFuturo,!!dinheiroNaConta);
-// Compare o total de habitantes do Brasil com China (valor em milhões)
-var brasil = 207;
-var china = 1340;
-var DiferencaBrasilChina = (china-brasil)*1000000000;
-console.log(DiferencaBrasilChina);
 
 
-// O que irá aparecer no console?
+// Qual valor é retornado na seguinte expressão? com && retorna o resultado da ultima expressão
+var expressao = (5 - 2) && (5 - ' ') && (5 - 3);
+console.log("expressao = ", expressao);
+//expressoa = 2
+
+// Verifique se as seguintes variáveis são True ou False
+var nome = 'Andre';          //true
+var idade = 28;              //true
+var possuiDoutorado = false; //false
+var empregoFuturo;           //false
+var dinheiroNaConta = 0;     //false
+var numero = 2;              //true
+console.log(!!nome,!!idade,!!possuiDoutorado,!!empregoFuturo,!!dinheiroNaConta, !!numero);
+//true true false false false true
+
+
+
+// O que irá aparecer no console? com && ambas as condições devem ser verdadeiras
 if(('Gato' === 'gato') && (5 > 2)) {
   console.log('Verdadeiro');
 } else {
   console.log('Gato != gato');
+  //Gato != gato
 }
 
-// O que irá aparecer no console?
+
+// O que irá aparecer no console? com || uma, das condições, deve ser verdadeira
 if(('Gato' === 'gato') || (5 > 2)) {
   console.log('Gato' && 'Cão');
   //Vai aparecer Cão pois no caso de comparação com && o último verdadeiro é que é impresso
@@ -88,4 +80,4 @@ if(('Gato' === 'gato') || (5 > 2)) {
   console.log('Falso');
 }
 
-//------------------------------------------------------------------------------
+
